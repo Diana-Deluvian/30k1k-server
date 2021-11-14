60b68fc66fe3f058f7ee115de85325e8758f882d
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-14 14:40:30 +0100

    fixed issue with UD routes

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
+const bcrypt = require("bcryptjs");
 
 
 require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(
+  cors( {
+    origin: ["http://localhost:3000", "http://192.168.1.234:3000"], // <-- location of the react app were connecting to
+    credentials: true,
+  })
+);
 
 const MongoStore = require('connect-mongo')(session);
 const PORT = process.env.PORT || 8080;
@@ -33,73 +40,49 @@ const Book = require('./Book');
 const Word = require('./Word');
 const User = require('./User');
 
-function validatePassword(password, hash, salt) {
-  var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-  return hash === hashVerify;
-}
-
-function genPassword(password) {
-  var salt = crypto.randomBytes(32).toString('hex');
-  var genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-  
-  return {
-    salt: salt,
-    hash: genHash
-  };
-}
-
-passport.use(new LocalStrategy(
-  function(username, password, cb) {
-    console.log("here1");
-    User.findOne({username})
-    .then((user) => {
-      if(!user) return cb(null, false)
-      console.log("here2");
-      const isValid = validatePassword(password, user.hash, user.salt);
-      isValid ? cb(null, user) : cb(null, false)
-    })
-    .catch((err) => console.log(err))
-  }
-));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((user, done) => {
-  User.findById(user.id)
-  .then((user) => {
-    done(null, user)
-  })
-  .catch((err) => done(err))
-});
-
-
 const sessionStore = new MongoStore({ mongooseConnection: connection, collection: 'sessions' });
 
 app.use(session({
   secret: process.env.SECRET,
-  resave: false,
+  resave: true,
   saveUninitialized: true,
   store: sessionStore,
   cookie: {
-      maxAge: 365 * 1000 * 60 * 60 * 24 // Equals 365 days (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+      maxAge: 365 * 1000 * 60 * 60 * 24 // Equals 365 days (24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
   }
 }));
 
+app.use(cookieParser(process.env.SECRET));
+
 app.use(passport.initialize());
 app.use(passport.session());
+require("./passportConfig")(passport);
+
+
+
 
 const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
       next();
   } else {
+    console.log(req);
       res.status(401).json({ msg: "Hey wait a second, you're not diana!" });
   }
 }
 
-app.post('/login', passport.authenticate('local',{ failureRedirect: '/login-failure', successRedirect: '/word' }), (err, req, res, next) => {
-  if (err) res.send(err);
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    console.log('here', user);
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
 });
 
 app.get('/logout', (req, res, next) => {
@@ -107,31 +90,31 @@ app.get('/logout', (req, res, next) => {
   res.send('logged out!');
 });
 
-/* 
-app.post('/register', (req, res, next) => {
-    
-  const saltHash = genPassword(req.body.password);
-  
-  const salt = saltHash.salt;
-  const hash = saltHash.hash;
-
-  const newUser = new User({
-      username: req.body.username,
-      hash: hash,
-      salt: salt
-  });
-
-  newUser.save()
-      .then((user) => {
-          console.log(user);
+ 
+app.post("/register", (req, res) => {
+  User.findOne({ username: req.body.username }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      console.log(hashedPassword);
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
       });
-
-  res.send("registered.");
-
+      await newUser.save();
+      res.send("User Created");
+    }
+  });
 });
-*/
 
-app.get('/word', (req, res) => {
+app.get("/user", (req, res) => {
+  console.log(req.user);
+  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
+});
+
+
+app.get('/words', (req, res) => {
   Word.find({}, (err, words) =>{
   if(err) return handleError(err)
   res.send(words)   
@@ -147,28 +130,30 @@ app.post('/word', isAuth, (req, res) => {
 });
 
 app.put('/word/:id', isAuth, (req, res) => {
-  const query = { "_id": req.params.id };
-  Word.updateOne(query)
-  .then(result => console.log(`updated ${result.updatedCount} item.`))
-  .catch(err => console.error(`update failed with error: ${err}`))
-});
-
-app.delete('/word/:id', isAuth, (req, res) => {
-  const query = { "_id": req.params.id };
-  Word.deleteOne(query)
-  .then(result => console.log(`Deleted ${result.deletedCount} item.`))
+  Word.findOneAndUpdate(
+    {_id: req.params.id},
+    req.body,
+    {returnDocument: 'after'})
+  .then(result => res.send(result))
   .catch(err => console.error(`Delete failed with error: ${err}`))
 });
 
-app.get('/book', (req, res) => {
+app.delete('/word/:id', isAuth, (req, res) => {
+  Word.deleteOne({_id: req.params.id})
+  .then(result => res.send(result))
+  .catch(err => console.error(`Delete failed with error: ${err}`))
+});
+
+app.get('/books', (req, res) => {
   Book.find({}, (err, books) =>{
   if(err) return handleError(err)
   res.send(books)   
   });
 })
 
-app.post('/book', isAuth, (req, res) => {   
+app.post('/book', isAuth, (req, res) => {  
   Book.create(req.body, function (err, book) {
+    console.log(err);
       if (err) return handleError(err);
       console.log(book)
       res.send(book);
@@ -176,19 +161,23 @@ app.post('/book', isAuth, (req, res) => {
 });
 
 app.put('/book/:id', isAuth, (req, res) => {
-  const query = { "_id": req.params.id };
-  Book.updateOne(query)
-  .then(result => console.log(`updated ${result.updatedCount} item.`))
-  .catch(err => console.error(`update failed with error: ${err}`))
-});
-
-app.delete('/book/:id', isAuth, (req, res) => {
-  const query = { "_id": req.params.id };
-  Book.deleteOne(query)
-  .then(result => console.log(`Deleted ${result.deletedCount} item.`))
+  Book.findOneAndUpdate(
+    {_id: req.params.id},
+    req.body,
+    {returnDocument: 'after'})
+  .then(result => res.send(result))
   .catch(err => console.error(`Delete failed with error: ${err}`))
 });
 
+app.delete('/book/:id', isAuth, (req, res) => {
+  Book.deleteOne({_id: req.params.id})
+  .then(result => res.send(result))
+  .catch(err => console.error(`Delete failed with error: ${err}`))
+});
+
+const handleError = (err) => {
+  console.log(err);
+}
 
 
 app.listen(8080, () => console.log('server is up and running!'));
